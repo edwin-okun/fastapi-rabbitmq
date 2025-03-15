b99d710afc6e92bb76bf2a2b73d035cbdc6edccc
edwin-okun <edwin.okun1@outlook.com>
2025-03-16 00:24:02 +0300

    Update SMS sending endpoint to accept Pydantic payload for improved validation and message formatting

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, status
 import pika
 
+from app.models.pydantic import SendSmsPayloadSchema
+
 app = FastAPI()
 
 # Define connection parameters once
@@ -11,7 +13,10 @@ params = pika.ConnectionParameters(
 )
 
 @app.post("/v1/sms", status_code=status.HTTP_200_OK)
-def send_sms():
+def send_sms(payload: SendSmsPayloadSchema):
+    # initialize body
+    body = None
+
     # Create a new connection for each request
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
@@ -19,9 +24,12 @@ def send_sms():
     # creating a queue
     channel.queue_declare(queue='hello')
 
+    # prepare body
+    body = payload.model_dump_json()
+    
     # send to queue
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
+    channel.basic_publish(exchange='', routing_key='hello', body=body)
+    print(f" [x] Sent {body}")
 
     # close connection
     connection.close()
